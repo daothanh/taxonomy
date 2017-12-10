99c2e87bd43c231383eb83288fd129f6b28aee48
Thanh Xuan Dao <thanhdx86@gmail.com>
2017-12-10 13:29:06 +0700

    Add api routes

---
+++ Http/apiRoutes.php
@@ -0,0 +1,95 @@
+<?php
+
+use Illuminate\Routing\Router;
+
+/** @var Router $router */
+
+//Vocabulary routes
+$router->group(['prefix' => 'vocabularies', 'middleware' => ['api.token', 'auth.admin']], function (Router $router) {
+	$router->bind('vocabulary', function ($id) {
+		return app('Modules\Taxonomy\Repositories\VocabularyRepository')->find($id);
+	});
+	$router->get('/', [
+		'as' => 'api.taxonomy.vocabulary.index',
+		'uses' => 'VocabularyController@index',
+		'middleware' => 'token-can:taxonomy.vocabularies.index',
+	]);
+	$router->post('/', [
+		'as' => 'api.taxonomy.vocabulary.store',
+		'uses' => 'VocabularyController@store',
+		'middleware' => 'token-can:taxonomy.vocabularies.create',
+	]);
+	$router->get('all', [
+		'as' => 'api.taxonomy.vocabulary.all',
+		'uses' => 'VocabularyController@all',
+		'middleware' => 'token-can:taxonomy.vocabularies.index',
+	]);
+	$router->post('find/{vocabulary}', [
+		'as' => 'api.taxonomy.vocabulary.find',
+		'uses' => 'VocabularyController@find',
+		'middleware' => 'token-can:taxonomy.vocabularies.edit',
+	]);
+
+	$router->post('{vocabulary}/edit', [
+		'as' => 'api.taxonomy.vocabulary.update',
+		'uses' => 'VocabularyController@update',
+		'middleware' => 'token-can:taxonomy.vocabularies.edit',
+	]);
+
+	$router->post('find-new', [
+		'as' => 'api.taxonomy.vocabulary.find-new',
+		'uses' => 'VocabularyController@findNew',
+		'middleware' => 'token-can:taxonomy.vocabularies.edit',
+	]);
+
+	$router->delete('{vocabulary}', [
+		'as' => 'api.taxonomy.vocabulary.destroy',
+		'uses' => 'VocabularyController@destroy',
+		'middleware' => 'token-can:taxonomy.vocabularies.destroy',
+	]);
+});
+
+//Term routes
+$router->group(['prefix' => 'terms', 'middleware' => ['api.token', 'auth.admin']], function (Router $router) {
+	$router->bind('term', function ($id) {
+		return app('Modules\Taxonomy\Repositories\TermRepository')->find($id);
+	});
+	$router->get('/', [
+		'as' => 'api.taxonomy.term.index',
+		'uses' => 'TermController@index',
+		'middleware' => 'token-can:taxonomy.terms.index',
+	]);
+	$router->post('/', [
+		'as' => 'api.taxonomy.term.store',
+		'uses' => 'TermController@store',
+		'middleware' => 'token-can:taxonomy.terms.create',
+	]);
+	$router->get('all', [
+		'as' => 'api.taxonomy.term.all',
+		'uses' => 'TermController@all',
+		'middleware' => 'token-can:taxonomy.terms.index',
+	]);
+	$router->post('find/{term}', [
+		'as' => 'api.taxonomy.term.find',
+		'uses' => 'TermController@find',
+		'middleware' => 'token-can:taxonomy.terms.edit',
+	]);
+
+	$router->post('{term}/edit', [
+		'as' => 'api.taxonomy.term.update',
+		'uses' => 'TermController@update',
+		'middleware' => 'token-can:taxonomy.terms.edit',
+	]);
+
+	$router->post('find-new', [
+		'as' => 'api.taxonomy.term.find-new',
+		'uses' => 'TermController@findNew',
+		'middleware' => 'token-can:taxonomy.terms.edit',
+	]);
+
+	$router->delete('{term}', [
+		'as' => 'api.taxonomy.term.destroy',
+		'uses' => 'TermController@destroy',
+		'middleware' => 'token-can:taxonomy.terms.destroy',
+	]);
+});
